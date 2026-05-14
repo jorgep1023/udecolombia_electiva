d976e5b9d04d7330f34b90d5f523e305114a547a
jorgep1023 <zero1023@gmail.com>
2026-05-14 12:32:47 -0500

    Update 01_introduccion.py

--- 01_introduccion.py
+++ 01_introduccion.py
@@ -1,3 +1,17 @@
 # Introduccion Dia 1 
 
 print("Hola Mundo")
+
+#---------------------
+#Capturar texto desde teclado: función input
+nombre = input("cómo te llamas: ")
+print("hola " + nombre)
+print("")
+
+nombre = str(input("cómo te llamas: "))
+edad = int(input("cual es tu edad : "))
+nota = float(input("cual es tu nota : "))
+
+print("hola " , nombre , ",tu edad es: ", edad , ",y tu nota es: ", nota)
+
+print("")
